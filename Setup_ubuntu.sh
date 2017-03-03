fddc02ff64d5aa9904c2755ecb1c0d3901f79831
mwiacx <759046501@qq.com>
2017-03-03 16:10:19 +0800

    update vimrc

---
+++ Setup_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##########################
+#Init a new ubuntu OS
+#author: mwiacx
+#date: 2017/2/27
+##########################
+
+#add some surces and update
+sudo apt-add-repository ppa: 
